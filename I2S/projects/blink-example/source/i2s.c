e0f20124b577d966dc655f95cd7297925c221acd
jmestanza <jmestanza@itba.edu.ar>
2020-02-01 20:03:33 -0300

    finished structures

--- I2S/projects/blink-example/source/i2s.c
+++ I2S/projects/blink-example/source/i2s.c
@@ -5,4 +5,15 @@
  *      Author: joa-m
  */
 
+#include "i2s.h"
 
+
+I2S_Type * i2s_ptr = I2S0;
+
+
+
+i2s_init(){
+	tx_config_regs tx_cfg;
+	tx_cfg.tx_cfg_0_reg.bit_clock_enable = true; //example
+
+}
--- I2S/projects/blink-example/source/i2s.h
+++ I2S/projects/blink-example/source/i2s.h
@@ -8,10 +8,10 @@
 #ifndef I2S_H_
 #define I2S_H_
 
+#include "MK64F12.h"
 
-
-typedef struct{
-	bool transmitter_enable;
+typedef struct {
+	bool rx_tx_enable;
 	bool stop_enable;
 	bool debug_enable;
 	bool bit_clock_enable;
@@ -29,12 +29,11 @@ typedef struct{
 	bool fifo_request_interrupt_enable;
 	bool fifo_warning_dma_enable;
 	bool fifo_request_dma_enable;
-} i2sx_tcsr_config;
+} i2sx_tx_rx_cr0_config;
 
 typedef struct {
 	uint32_t transmit_fifo_watermark; // (3 bits - maxn 7)
-} i2sx_tcr1_config;
-
+} i2sx_tx_rx_cr1_config;
 
 typedef struct {
 	uint32_t synchronous_mode; // 2 bits
@@ -44,13 +43,13 @@ typedef struct {
 	bool bit_clock_polarity;
 	bool bit_clock_direction;
 	uint8_t bit_clock_divide; // 8 bits
-} i2sx_tcr2_config;
+} i2sx_tx_rx_cr2_config;
 
 typedef struct {
 	bool transmit_channel_1_enable; // bit 17
 	bool transmit_channel_2_enable; // bit 16
 	uint32_t word_flag_configuration;  // 5 bits
-} i2sx_tcr3_config;
+} i2sx_tx_rx_cr3_config;
 
 typedef struct {
 	uint32_t frame_size; // bits 20-16 incluidos / 5 bits
@@ -59,7 +58,45 @@ typedef struct {
 	bool frame_sync_early;
 	bool frame_sync_polarity;
 	bool frame_sync_direction;
-} i2sx_tcr4_config;
+} i2sx_tx_rx_cr4_config;
+
+typedef struct {
+	uint32_t word_n_width; // 5 bits
+	uint32_t word_0_width; // 5 bits
+	uint32_t first_bit_shifted; // 5 bits
+
+} i2sx_tx_rx_cr5_config;
+
+typedef struct {
+	uint32_t write_fifo_pointer;
+	uint32_t read_fifo_pointer;
+} i2sx_tx_rx_fr_n;
+
+typedef struct {
+	i2sx_tx_rx_cr0_config tx_cfg_0_reg; // (3 bits - maxn 7)
+	i2sx_tx_rx_cr1_config tx_cfg_1_reg;
+	i2sx_tx_rx_cr2_config tx_cfg_2_reg;
+	i2sx_tx_rx_cr3_config tx_cfg_3_reg;
+	i2sx_tx_rx_cr4_config tx_cfg_4_reg;
+	i2sx_tx_rx_cr5_config tx_cfg_5_reg;
+	uint32_t tx_data_reg[2];
+	i2sx_tx_rx_fr_n tx_fifo_reg[2];
+	uint32_t tx_word_mask; // 0 = word N enabled ; 1 = word N is masked (The transmit data pins are tri-stated when masked.)
+} tx_config_regs;
+
+typedef struct {
+	i2sx_tx_rx_cr0_config rx_cfg_0_reg; // (3 bits - maxn 7)
+	i2sx_tx_rx_cr1_config rx_cfg_1_reg;
+	i2sx_tx_rx_cr2_config rx_cfg_2_reg;
+	i2sx_tx_rx_cr3_config rx_cfg_3_reg;
+	i2sx_tx_rx_cr4_config rx_cfg_4_reg;
+	i2sx_tx_rx_cr5_config rx_cfg_5_reg;
+	uint32_t rx_data_reg[2];
+	i2sx_tx_rx_fr_n rx_fifo_reg[2];
+	uint32_t rx_word_mask; // 0 = word N enabled ; 1 = word N is masked (The transmit data pins are tri-stated when masked.)
+} rx_config_regs;
+
+void i2s_init(void);
 
 
 #endif /* I2S_H_ */
